c68b01f91e52f1c8e08b27033a1fbd7c5052466d
Erlingus <munguia.erling4@gmail.com>
2023-02-20 22:59:55 +0000

    updated

--- contact.php
+++ contact.php
@@ -20,27 +20,61 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-  
         <div class="collapse navbar-collapse" id="navbarsExample07">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           </ul>
           <span class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-1.php">Page 1</a>
+                <a class="nav-link" aria-current="page" href="page-1.html">Page 1</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-2.php">Page 2</a>
+                <a class="nav-link" aria-current="page" href="page-2.html">Page 2</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="page-3.php">Page 3</a>
+                <a class="nav-link" aria-current="page" href="page-3.html">Page 3</a>
               </li>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="contact.php">CONTACT</a>
-              </li>
             </ul>
           </span>
         </div>
       </div>
     </nav>
+
+<main class="container">
+  <div class="starter-template text-center">
+    <h1>CONTACT</h1>
+    <p class="lead">THE CONTACT FORM WILL BE HERE
+
+
+    <form method="POST" name="contactform" action="contact-form-handler.php"> 
+<p>
+<label for='name'>Your Name:</label> <br>
+<input type="text" name="name">
+</p>
+<p>
+<label for='email'>Email Address:</label> <br>
+<input type="text" name="email"> <br>
+</p>
+
+<p>
+<label for="phone">Enter a phone number:</label><br><br>
+  <input type="tel" name="phone" 
+  placeholder="123-45-678" 
+  required><br><br>
+</p>
+
+<p>
+<label for='message'>Message:</label> <br>
+<textarea name="message"></textarea>
+</p>
+<input type="submit" value="Submit"><br>
+</form>
+<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</main><!-- /.container -->
+<script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
+
+
