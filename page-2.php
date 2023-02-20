3c54a8c3497497c78b1d792bb09cf126430f39f0
Erlingus <munguia.erling4@gmail.com>
2023-02-20 22:26:35 +0000

    updated

--- page-2.php
+++ page-2.php
@@ -1,14 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.80.0">
-    <title>Starter Template · Bootstrap v5.0</title>
-
-
+<?php include 'includes/header.php';?>
 <main class="container">
   <div class="starter-template text-center">
     <h1>Page 2</h1>
@@ -16,6 +6,4 @@
   </div>
 
 </main><!-- /.container -->
-    <script src="js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>
+<?php include 'includes/footer.php';?>
