7dcd1c25b9313a3616011dd3e9f13db1d2ea5c29
Erlingus <munguia.erling4@gmail.com>
2023-02-24 23:36:20 +0000

    updated

--- includes/header.php
+++ includes/header.php
@@ -21,17 +21,20 @@
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
       <div class="container">
-        <a class="navbar-brand" href="index.php">Site Title</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
+        
+      <!-- <img src="images/dental7.jpg" alt=""> -->
 
         <div class="collapse navbar-collapse" id="navbarsExample07">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           </ul>
           <span class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <a class="navbar-brand" href="index.php">Home</a>
+        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button> -->
+
               <li class="nav-item">
                 <a class="nav-link" aria-current="page" href="page-1.php">Page 1</a>
               </li>
@@ -43,7 +46,7 @@
               </li>
               </li>
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="contact.php">CONTACT</a>
+                <a class="nav-link" aria-current="page" href="contact.php">Contact</a>
               </li>
             </ul>
           </span>
