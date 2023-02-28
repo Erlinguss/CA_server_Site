aab405e96222e87f561707ecf81a76f79886f55d
Erlingus <munguia.erling4@gmail.com>
2023-02-28 16:00:34 +0000

    updated

--- page-1.php
+++ page-1.php
@@ -25,87 +25,48 @@ $statement->closeCursor();
 
 
 <?php include 'includes/header.php';?>
-<div class="container">
-<pre>
-    <code>
-      &lt;?php
+
 <main >
 
-<!-- <div class="container" > -->
-
-<section>
-        <!-- display a table of doctors -->
-      
-    
-        <h1>Doctor Table </h1>
-            <!-- <tr> -->
-                <!-- <th>Doctor_ID</th> -->
-<!--             
-                <th>First_Name</th>
-                <th>SurName</th>
-                <th>Address</th>
-                <th>Telephone</th>
-                <th>Email</th>
-                <th>Delete</th> -->
-            <!-- </tr> -->
-
+<div class="container" >
+        <h1>Doctor Data </h1>
+        
             <?php foreach ($doctors as $doctor) : ?>
-            <!-- <tr> -->
-             
                 <?php echo $doctor['First_Name_Doctor']; ?>
                 <?php echo $doctor['Last_Name_Doctor']; ?>
                  <?php echo $doctor['Doctor_Address']; ?>
                 <?php echo $doctor['Doctor_Telephone']; ?>
                 <?php echo $doctor['Doctor_Email']; ?>
-                <form action="contact.php" >
+          
+                <form action="contact.php"   method="post">
                         <!-- <input type="hidden" name="record_type" value="doctor"> -->
                         <!-- <input type="hidden" name="record_id" value="<?php echo $doctor['Doctor_Id']; ?>"> -->
                         <input type="submit" value="Booking">
                     </form>
-<!-- </td> -->
-            <!-- </tr> -->
+          
+
             <?php endforeach; ?>
-        <!-- </table> -->
-    ?&gt;
-    </code>
-  </pre>
-</div>
-
     </div >
-    <div class="container">
-   <section>
-        <!-- display a table of clinic -->
-        <!-- <table> -->
-            <h1>Clinic Table </h1>
-            <!-- <tr> -->
-                <!-- <th>Clinic_ID</th> -->
-                <!-- <th>Clinic_Name</th>
-                <th>Address</th>
-                <th>Telephone</th>
-                <th>Email</th>
-                <th>Booking</th>
-            </tr> -->
 
+    <div class="container">
+
+            <h1>Clinic Data </h1>
+        
             <?php foreach ($clinics as $clinic) : ?>
-            <!-- <tr> -->
                 <?php echo $clinic['Clinic_Name']; ?>
                 <?php echo $clinic['Clinic_Address']; ?>
                 <?php echo $clinic['Clinic_Telephone']; ?>
                 <?php echo $clinic['Clinic_Email']; ?>
-<div>
+
                 <form action="contact.php" method="post">
-    <input type="hidden" name="record_type" value="clinic">
-    <input type="hidden" name="record_id" value="<?php echo $clinic['Clinic_Id']; ?>">
+    <!-- <input type="hidden" name="record_type" value="clinic"> -->
+    <!-- <input type="hidden" name="record_id" value="<?php echo $clinic['Clinic_Id']; ?>"> -->
     <input type="submit" value="Booking">
 </form>
-            </div>
-
-            <!-- </tr> -->
             <?php endforeach; ?>
-        <!-- </table> -->
-    </section>
+          
+   
     </div>
-    
 
 </main>
 <?php include 'includes/footer.php';?>
