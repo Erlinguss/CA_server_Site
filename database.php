255b91a9115edf4257c78ead59bc321ba7e9c705
Erlingus <munguia.erling4@gmail.com>
2023-03-06 15:07:00 +0000

    updated

--- database.php
+++ database.php
@@ -1,12 +1,12 @@
 <?php
-     $dsn = 'mysql:host=localhost;dbname=d00251914';
-    $username = 'root';
-    $password = '';
+    //  $dsn = 'mysql:host=localhost;dbname=d00251914';
+    // $username = 'root';
+    // $password = '';
 
     // This code is for the DKIT server site
-    // $dsn = 'mysql:host=localhost;dbname=d00251914';
-    // $username = 'd00251914';
-    // $password = 'jQ46eve2';
+    $dsn = 'mysql:host=localhost;dbname=d00251914';
+    $username = 'd00251914';
+    $password = 'jQ46eve2';
     
 
     try {
