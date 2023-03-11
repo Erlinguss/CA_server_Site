70295e2e1198bd1a4ca2b4f02ed3f1c28850cb39
Erlingus <munguia.erling4@gmail.com>
2023-03-11 19:39:28 +0000

    updated

--- contact-form-thank-you.php
+++ contact-form-thank-you.php
@@ -8,10 +8,10 @@
 
   
   <h1 class="mt-10">Thank you!</h1>
-<h2>Thank you for submitting the form</h2>
-<h2>We appreciate your interest in our Clinic and will review your information shortly. Our team will be in touch with you soon to follow up on your inquiry.</h2>
-<h2>In the meantime, please feel free to explore our website to learn more about our services and what we can offer you. If you have any questions or concerns, please don't hesitate to contact us directly. We are always here to help.</h2>
-<h2>Thank you again for choosing our clinic, and we look forward to speaking with you soon!</h2>
+<h7>Thank you for submitting the form</h7>
+<h7>We appreciate your interest in our Clinic and will review your information shortly. Our team will be in touch with you soon to follow up on your inquiry.</h7>
+<h7>In the meantime, please feel free to explore our website to learn more about our services and what we can offer you. If you have any questions or concerns, please don't hesitate to contact us directly. We are always here to help.</h7>
+<h7>Thank you again for choosing our clinic, and we look forward to speaking with you soon!</h7>
   </div>
 
 </main><!-- /.container -->
